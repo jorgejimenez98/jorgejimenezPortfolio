4144be035a332e22097ce889cc7625398e627418
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-09-23 00:23:02 +0200

    ADDED new routes to the app

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import Main from "./components/Main";
 import Contact from "./components/Contact";
+import AboutMe from "./components/AboutMe";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 
 export const routes = [
-    { path: "/home", component: Main, exact: true },
-    { path: "/contact", component: Contact },
+  { path: "/home", component: Main, exact: true },
+  { path: "/contact", component: Contact },
+  { path: "/about", component: AboutMe },
+  { path: "/skills", component: Skills },
+  { path: "/projects", component: Projects },
+  { path: "/experience", component: Experience },
 ];
