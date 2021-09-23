f5c285185b01acd7063b1c558924b184a04b9d31
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-09-23 16:41:32 +0200

    ADDED display visibility to others components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,42 +1,51 @@
-import React from "react";
-import SocialMedia from "./SocialMedia";
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import ContactSocialMedia from "./ContactSocialMedia";
+import ContactForm from "./ContactForm";
 import { contactInfo } from "../portfolio";
 import { Fade } from "react-reveal";
 import contactMailDark from "../assets/img/contactMaiDark.svg";
 
 export default function Contact() {
+  const [displayForm, setDisplayForm] = useState("none");
+  const [displaySocialMedia, setDisplaySocialMedia] = useState("inline");
+  const [buttomText, setButtomText] = useState("Send Direct Email");
+
+  const changeVisibility = () => {
+    if (displayForm === "none") {
+      setDisplayForm("inline");
+      setDisplaySocialMedia("none");
+      setButtomText("Go Back");
+    } else {
+      setDisplayForm("none");
+      setDisplaySocialMedia("inline");
+      setButtomText("Send Direct Email");
+    }
+  };
+
   return (
     <Fade bottom duration={1000} distance="20px">
       <div className="main contact-margin-top" id="contact">
         <div className="contact-div-main">
           <div className="contact-header">
             <h1 className="heading contact-title">{contactInfo.title}</h1>
-            <p className={"subTitle contact-subtitle"}>
-              {contactInfo.subtitle}
-            </p>
-            <div className={"contact-text-div"}>
-              <a className="contact-detail" href={"tel:" + contactInfo.number}>
-                Spain {contactInfo.number}
-              </a>
-              <br />
-              <br />
-              <a
-                className="contact-detail"
-                href={"tel:" + contactInfo.cubaNumber}
+
+            <div style={{ display: displaySocialMedia }}>
+              <ContactSocialMedia />
+            </div>
+
+            <div style={{ display: displayForm }}>
+              <ContactForm />
+            </div>
+
+            <div className="container button-direct mt-4 mr-2">
+              <Button
+                variant="text"
+                onClick={changeVisibility}
+                className="animated infinite pulse"
               >
-                Cuba {contactInfo.cubaNumber}
-              </a>
-              <br />
-              <br />
-              <a
-                className="contact-detail-email"
-                href={"mailto:" + contactInfo.emailAddress}
-              >
-                {contactInfo.emailAddress}
-              </a>
-              <br />
-              <br />
-              <SocialMedia />
+                {buttomText}
+              </Button>
             </div>
           </div>
           <div className="contact-image-div">
