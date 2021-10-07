43b7e5eca0956a067c28e8b18e24a3b2b3fddc99
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-07 05:16:16 +0200

    CREATE home component with greetings sections

---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import emoji from "react-easy-emoji";
+import SocialMedia from "./SocialMedia";
+import { greeting } from "../portfolio";
+
+function Home() {
+  return (
+    <div className="greet-main" id="greeting">
+      <div className="greeting-main">
+        <div className="greeting-text-div">
+          <div>
+            <h1 className={"greeting-text"}>
+              {greeting.title}
+              <span className="wave-emoji">{emoji("👋")}</span>
+            </h1>
+            <p className={"greeting-text-p subTitle"}>{greeting.subTitle}</p>
+            <SocialMedia />
+          </div>
+        </div>
+        <div className="greeting-image-div text-center">
+          <img
+            alt="man sitting on table"
+            className="image"
+            src={greeting.image}
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
