effd0ecfc0fe4e6026b90df465c5184af8625cd9
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-07 05:15:16 +0200

    FNISH experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,34 +1,18 @@
 import React from "react";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
+import { VerticalTimeline } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import WorkIcon from "@material-ui/icons/Work";
 import ExperienceElement from "./ExperienceElement";
+import { experience } from "../portfolio";
 
 function Experience() {
   return (
-    <VerticalTimeline>
-      <ExperienceElement />
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        date="2010 - 2011"
-        contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        dateClassName="text-dark"
-        icon={<WorkIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Art Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          San Francisco, CA
-        </h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, SEO, Online
-          Marketing
-        </p>
-      </VerticalTimelineElement>
-    </VerticalTimeline>
+    <React.Fragment>
+      <h1 className="text-center mb-4">Work Experience</h1>
+      <VerticalTimeline>
+        <ExperienceElement experience={experience.freelance} />
+        <ExperienceElement experience={experience.ucm} />
+      </VerticalTimeline>
+    </React.Fragment>
   );
 }
 
