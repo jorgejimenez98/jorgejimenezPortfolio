fa7db5aa7a9e81569b9eb0004115c964624a6287
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-07 03:22:59 +0200

    INIT experience component with vertical animations

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import WorkIcon from "@material-ui/icons/Work";
+import ExperienceElement from "./ExperienceElement";
 
 function Experience() {
-    return (
-        <div>
-            Experience
-        </div>
-    )
+  return (
+    <VerticalTimeline>
+      <ExperienceElement />
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2010 - 2011"
+        contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        dateClassName="text-dark"
+        icon={<WorkIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Art Director</h3>
+        <h4 className="vertical-timeline-element-subtitle">
+          San Francisco, CA
+        </h4>
+        <p>
+          Creative Direction, User Experience, Visual Design, SEO, Online
+          Marketing
+        </p>
+      </VerticalTimelineElement>
+    </VerticalTimeline>
+  );
 }
 
-export default Experience
+export default Experience;
