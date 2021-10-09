5445a8c8a0f04fe4017ef72f6800a0958246f2d9
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-09 23:46:11 +0200

    ADD component Experience to main routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Main from "./views/Main";
 import Contact from "./views/Contact";
+import Experience from "./views/Experience";
 
 export const routes = [
   { path: "/home", component: Main, exact: true },
   { path: "/contact", component: Contact },
+  { path: "/experience", component: Experience },
 ];
