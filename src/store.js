08b51693c2303b9a665421bdffedca106f0813c7
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-09 17:18:56 +0200

    ADD initial state on languaje reducer store

--- src/store.js
+++ src/store.js
@@ -7,6 +7,31 @@ import { snackbarReducer } from "./redux/reducers/snackBarReducer";
 import { emailSendReducer } from "./redux/reducers/emailReducers";
 import { languajeReducer } from "./redux/reducers/languajeReducers";
 
+// Languajes
+import { portfolioEn } from "./portfolio/en";
+import { portfolioEs } from "./portfolio/es";
+
+// Check if there is a languaje on localStorage
+
+const lang = localStorage.getItem("languaje")
+  ? localStorage.getItem("languaje")
+  : null;
+
+let initialState = { languaje: { languaje: "", portfolio: {} } };
+if (lang === null) {
+  initialState.languaje.languaje = "en";
+  initialState.languaje.portfolio = portfolioEn;
+  localStorage.setItem("languaje", "en");
+} else if (lang === "en") {
+  initialState.languaje.languaje = "en";
+  initialState.languaje.portfolio = portfolioEn;
+} else {
+  initialState.languaje.languaje = "es";
+  initialState.languaje.portfolio = portfolioEs;
+}
+
+// Combine Reducers
+
 const reducer = combineReducers({
   snackbar: snackbarReducer,
   emailSend: emailSendReducer,
@@ -17,7 +42,7 @@ const middleware = [thunk];
 
 const store = createStore(
   reducer,
-  {},
+  initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
