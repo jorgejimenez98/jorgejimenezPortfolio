3583f9812c6606b2425cafe638d3abfd0ff5ad4c
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-09 02:53:14 +0200

    ADD languaje reducer to redux store

--- src/store.js
+++ src/store.js
@@ -5,10 +5,12 @@ import { composeWithDevTools } from "redux-devtools-extension";
 // REDUCERS
 import { snackbarReducer } from "./redux/reducers/snackBarReducer";
 import { emailSendReducer } from "./redux/reducers/emailReducers";
+import { languajeReducer } from "./redux/reducers/languajeReducers";
 
 const reducer = combineReducers({
   snackbar: snackbarReducer,
   emailSend: emailSendReducer,
+  languaje: languajeReducer,
 });
 
 const middleware = [thunk];
