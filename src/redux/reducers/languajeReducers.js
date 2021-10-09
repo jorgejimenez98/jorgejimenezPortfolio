f729227087b51c3c6d857f899bd429060f73c4e0
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-09 17:18:33 +0200

    UPDATE reducer, initial state is empty

--- src/redux/reducers/languajeReducers.js
+++ src/redux/reducers/languajeReducers.js
@@ -1,12 +1,6 @@
 import { CHANGE_LANGUAJE_MESSAGES } from "../constants/languajeConstants";
-import { portfolioEn } from "../../portfolio/en";
 
-const initialState = {
-  languaje: "en",
-  portfolio: portfolioEn,
-};
-
-export const languajeReducer = (state = initialState, action) => {
+export const languajeReducer = (state = {}, action) => {
   switch (action.type) {
     case CHANGE_LANGUAJE_MESSAGES:
       const { languaje, portfolio } = action.payload;
