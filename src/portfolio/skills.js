d5cbb2db2210fa1e64415b70ba7af3ce415fab13
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-10 02:41:00 +0200

    CREATE skills arrays

---
+++ src/portfolio/skills.js
@@ -0,0 +1,111 @@
+import PostgreSQL from "../assets/icons/postgresql.svg";
+import MYSQL from "../assets/icons/mysql.png";
+import REST from "../assets/icons/rest-frame.png";
+import REDUX from "../assets/icons/redux.png";
+import MONGODB from "../assets/icons/mongodb.png";
+import GRAPHQL from "../assets/icons/graphql.png";
+import APOLLO from "../assets/icons/apollo.png";
+import SQLSERVER from "../assets/icons/sqlserver.png";
+import LINUX from "../assets/icons/linux.png";
+import Django from "../assets/icons/django.png";
+
+export const skills = [
+  // LANGUAJES
+  {
+    skillName: "JavaScript",
+    fontAwesomeClassname: "fab fa-js",
+  },
+  {
+    skillName: "Python",
+    fontAwesomeClassname: "fab fa-python",
+  },
+  {
+    skillName: "Java",
+    fontAwesomeClassname: "fab fa-java",
+  },
+  {
+    skillName: "Android",
+    fontAwesomeClassname: "fab fa-android",
+  },
+  {
+    skillName: "REST API",
+    icon: REST,
+  },
+  // FRAMEWORKS
+  {
+    skillName: "Angular",
+    fontAwesomeClassname: "fab fa-angular",
+  },
+
+  {
+    skillName: "reactjs",
+    fontAwesomeClassname: "fab fa-react",
+  },
+  {
+    skillName: "nodejs",
+    fontAwesomeClassname: "fab fa-node",
+  },
+  {
+    skillName: "Django",
+    icon: Django,
+  },
+  // DESIGN
+  {
+    skillName: "css3",
+    fontAwesomeClassname: "fab fa-css3-alt",
+  },
+  {
+    skillName: "bootstrap",
+    fontAwesomeClassname: "fab fa-bootstrap",
+  },
+  {
+    skillName: "html-5",
+    fontAwesomeClassname: "fab fa-html5",
+  },
+  // DATABASES
+  {
+    skillName: "sql-database",
+    fontAwesomeClassname: "fas fa-database",
+  },
+  {
+    skillName: "MySQL",
+    icon: MYSQL,
+  },
+  {
+    skillName: "PostgreSQL",
+    icon: PostgreSQL,
+  },
+  {
+    skillName: "MongoDB",
+    icon: MONGODB,
+  },
+  {
+    skillName: "SQL Server 2008",
+    icon: SQLSERVER,
+  },
+  // OTHERS
+  {
+    skillName: "docker",
+    fontAwesomeClassname: "fab fa-docker",
+  },
+  {
+    skillName: "npm",
+    fontAwesomeClassname: "fab fa-npm",
+  },
+  {
+    skillName: "ReduxJS",
+    icon: REDUX,
+  },
+  {
+    skillName: "GraphQL",
+    icon: GRAPHQL,
+  },
+  {
+    skillName: "Apollo.js",
+    icon: APOLLO,
+  },
+  {
+    skillName: "Linux",
+    icon: LINUX,
+  },
+];
