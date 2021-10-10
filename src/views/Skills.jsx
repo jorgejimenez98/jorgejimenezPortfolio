cd65814effb030f3503148f127d35cc0ac8dd2ee
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-10 02:42:22 +0200

    UPDATE skills component, reduce code

--- src/views/Skills.jsx
+++ src/views/Skills.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Fade } from "react-awesome-reveal";
+import { skills } from "../portfolio/skills"
 
-function Skills() {
-    return (
-        <div>
-            Skills
+// Components
+import SkillList from "../components/Skills/SkillList";
+import SkillsMainSkills from "../components/Skills/SkillsMainSkills";;
+
+export default function Skills() {
+  const { portfolio } = useSelector((state) => state.languaje);
+
+  return (
+    <Fade bottom duration={1000} distance="20px">
+      <div className="skills-container">
+        <SkillsMainSkills portfolio={portfolio} />
+
+        <div className="software-skills-main-div ml-2">
+          <SkillList title={portfolio.techStack.tools} list={skills} />
         </div>
-    )
+      </div>
+    </Fade>
+  );
 }
-
-export default Skills
