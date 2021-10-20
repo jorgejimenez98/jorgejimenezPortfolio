b8f61911d562089859c06b914f296c2f8f02cf7f
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-10-20 05:25:10 +0200

    CREATE dialog settings index exports

---
+++ src/components/CVDialog/settings/index.js
@@ -0,0 +1,5 @@
+import { BootstrapDialogTitle } from "./BootstrapDialogTitle";
+import { BootstrapDialog } from "./bootstrapdialog";
+import { Transition } from "./transition";
+
+export { BootstrapDialog, BootstrapDialogTitle, Transition };
