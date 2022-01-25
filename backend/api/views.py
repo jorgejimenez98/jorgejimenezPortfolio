69dbafb67012781e792b84dff01153fab890251a
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-25 19:04:26 +0100

    Create get site config view functions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from .serializers import SiteConfiguration, SiteConfigurationSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def getSiteConfigurations(request):
+    try:
+        config = SiteConfiguration.objects.get()
+        serializer = SiteConfigurationSerializer(config)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({"detail": e.args[0]}, status=status.HTTP_400_BAD_REQUEST)
