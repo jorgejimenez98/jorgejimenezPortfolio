4e49d98a4b26fd5e2a6921c70d2487fb0f2c2746
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-25 18:53:49 +0100

    Create site configuration model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from solo.models import SingletonModel
 
-# Create your models here.
+# Site configuration Model
+
+class SiteConfiguration(SingletonModel):
+    developer_name = models.CharField(max_length=255, default="")
+    main_presentation_text = models.TextField(default="")
+    years_of_experience = models.PositiveIntegerField(default=0)
+    is_open_to_work = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=255, default="")
+    repository_project_url = models.URLField(default="")
+    developer_email = models.EmailField(default="")
+
+    def __str__(self):
+        return "Site Configuration"
+
+    class Meta:
+        verbose_name = "Site Configuration"
