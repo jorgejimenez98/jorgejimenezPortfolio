06e3ddca8a7f41abe3045faf8981064cfb201992
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-30 13:45:51 +0100

    Add create method on tech view set

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
+from django.core.files.storage import FileSystemStorage
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
@@ -37,3 +38,19 @@ class Main_TechViewSet(viewsets.ModelViewSet):
 class TechnologyViewSet(viewsets.ModelViewSet):
     queryset = Technologie.objects.all().order_by('-pk')
     serializer_class = TechnologieSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        try:
+            """ Create New Tech """
+            newTech = Technologie.objects.create(
+                name=data.get('name'),
+                image_logo=data.get('image_logo'),
+            )
+            """ Save Image FIle """
+            fs = FileSystemStorage()
+            fs.save(newTech.image_logo.name, data.get('image_logo'))
+            """ Return Response """
+            return Response(TechnologieSerializer(newTech).data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'detail': e.args[0]}, status=status.HTTP_400_BAD_REQUEST)
