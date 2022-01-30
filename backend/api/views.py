1271413b15257982b3429b219c83b3a9b9cf9f92
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-30 13:27:11 +0100

    Create main tech and tech view set

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,6 +6,7 @@ from .serializers import *
 
 """ Site Configuration View """
 
+
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def getSiteConfigurations(request):
@@ -16,8 +17,23 @@ def getSiteConfigurations(request):
     except Exception as e:
         return Response({"detail": e.args[0]}, status=status.HTTP_400_BAD_REQUEST)
 
+
 """ Social Media View Set """
+
 
 class SocialMediaViewSet(viewsets.ModelViewSet):
     queryset = SocialMedia.objects.all().order_by('-pk')
     serializer_class = SocialMediaSerializer
+
+
+""" Technology View Set """
+
+
+class Main_TechViewSet(viewsets.ModelViewSet):
+    queryset = Main_Tech.objects.all().order_by('-pk')
+    serializer_class = Main_TechSerializer
+
+
+class TechnologyViewSet(viewsets.ModelViewSet):
+    queryset = Technologie.objects.all().order_by('-pk')
+    serializer_class = TechnologieSerializer
