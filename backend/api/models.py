8da47423840e707fbdd542c2830f2c586f830064
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-30 13:26:37 +0100

    Create main tech and tech model

--- backend/api/models.py
+++ backend/api/models.py
@@ -32,3 +32,23 @@ class SocialMedia(models.Model):
 
     def __str__(self):
         return f'{self.className} - {self.link}'
+
+
+""" Technologie """
+
+
+class Main_Tech(models.Model):
+    text = models.CharField(max_length=255)
+    proccessPorcent = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Main Tech {self.text} - {self.proccessPorcent}'
+
+
+def uploadTechPath(x, filename):
+    return '/'.join(['technology', filename])
+
+
+class Technologie(models.Model):
+    name = models.CharField(max_length=255)
+    image_logo = models.ImageField(blank=True, null=True, upload_to=uploadTechPath)
