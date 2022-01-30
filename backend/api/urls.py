de703562f784dd50427e5c28ceee9ec04987d318
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-30 12:45:35 +0100

    Register social media view set to main router

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 from django.conf.urls import include
 from django.urls import path
-from .views import getSiteConfigurations
+from .views import getSiteConfigurations, SocialMediaViewSet
 
 router = routers.DefaultRouter()
+router.register('socialMedias', SocialMediaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
