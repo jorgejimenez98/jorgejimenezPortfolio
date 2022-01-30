85200c5622055541030d64ac3ceeef9fcb4dbb92
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-30 12:44:48 +0100

    Create social media view set

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,9 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from .serializers import SiteConfiguration, SiteConfigurationSerializer
+from .serializers import *
 
+""" Site Configuration View """
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -14,3 +15,9 @@ def getSiteConfigurations(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({"detail": e.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+
+""" Social Media View Set """
+
+class SocialMediaViewSet(viewsets.ModelViewSet):
+    queryset = SocialMedia.objects.all().order_by('-pk')
+    serializer_class = SocialMediaSerializer
