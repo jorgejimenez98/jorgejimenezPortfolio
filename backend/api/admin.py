34acbc57fdceec82b1b3deadee8ae5a7e44ea62e
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-30 13:27:25 +0100

    Add main tech and tech model to django admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from solo.admin import SingletonModelAdmin
-from .models import SiteConfiguration, SocialMedia
+from .models import SiteConfiguration, SocialMedia, Main_Tech, Technologie
 
 
 admin.site.register(SiteConfiguration, SingletonModelAdmin)
 admin.site.register(SocialMedia)
+admin.site.register(Main_Tech)
+admin.site.register(Technologie)
