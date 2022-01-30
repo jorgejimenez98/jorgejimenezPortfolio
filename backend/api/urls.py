fa03a44beb5af19570174151225ab86dbe9726b0
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-30 13:27:38 +0100

    Add main tech and tech view set to rest router

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,12 @@
 from rest_framework import routers
 from django.conf.urls import include
 from django.urls import path
-from .views import getSiteConfigurations, SocialMediaViewSet
+from .views import getSiteConfigurations, SocialMediaViewSet, Main_TechViewSet, TechnologyViewSet
 
 router = routers.DefaultRouter()
 router.register('socialMedias', SocialMediaViewSet)
+router.register('technologies', TechnologyViewSet)
+router.register('mainTech', Main_TechViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
