2307f28edbaacf7eb79aa875147249ba304957c2
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-30 12:45:02 +0100

    Add social media model to admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from solo.admin import SingletonModelAdmin
-from .models import SiteConfiguration
+from .models import SiteConfiguration, SocialMedia
 
 
 admin.site.register(SiteConfiguration, SingletonModelAdmin)
+admin.site.register(SocialMedia)
