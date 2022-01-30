b14750239b6c7f55d081173ad78045bc6ed65db5
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-30 12:44:37 +0100

    Create social media serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import SiteConfiguration
+from .models import SiteConfiguration, SocialMedia
+
+
+""" Site Configuration Serializer """
 
 
 class SiteConfigurationSerializer(serializers.ModelSerializer):
     class Meta:
         model = SiteConfiguration
         fields = "__all__"
+
+
+""" Social Media Serializer """
+
+
+class SocialMediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialMedia
+        fields = '__all__'
