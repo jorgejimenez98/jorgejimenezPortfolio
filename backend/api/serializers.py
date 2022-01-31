734859f9bd6ac3a953bdea95a2d01ba6b27d4af9
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-31 15:51:08 +0100

    Create project serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import SiteConfiguration, SocialMedia, Main_Tech, Technologie, Curriculum
+from .models import SiteConfiguration, SocialMedia, Main_Tech, Technologie, Curriculum, ProjectItem, Project
 
 
 """ Site Configuration Serializer """
@@ -42,3 +42,38 @@ class CurriculumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curriculum
         fields = '__all__'
+
+
+""" Projects Serializers """
+
+
+class ProjectItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectItem
+        fields = '__all__'
+
+
+class ProjectMiniSerializer(serializers.ModelSerializer):
+    technologies = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'description', 'technologies']
+
+    def get_technologies(self, obj):
+        techs = Technologie.objects.filter(project__id=obj.pk)
+        serializer = TechnologieSerializer(techs)
+        return serializer.data
+
+
+class ProjectSerializer(ProjectMiniSerializer):
+    projectItems = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'description', 'technologies', 'projectItems']
+
+    def get_projectItems(self, obj):
+        projectItems = obj.projectItems.all()
+        serializer = ProjectItemSerializer(projectItems)
+        return serializer.data
