8b9ecebe9891f1196a1ab8b3a099634c94e18ece
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-31 15:21:51 +0100

    Add Curriculum model to rest routers

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,13 @@
 from rest_framework import routers
 from django.conf.urls import include
 from django.urls import path
-from .views import getSiteConfigurations, SocialMediaViewSet, Main_TechViewSet, TechnologyViewSet
+from .views import getSiteConfigurations, SocialMediaViewSet, Main_TechViewSet, TechnologyViewSet, CurriculumViewSet
 
 router = routers.DefaultRouter()
 router.register('socialMedias', SocialMediaViewSet)
 router.register('technologies', TechnologyViewSet)
 router.register('mainTech', Main_TechViewSet)
+router.register('curriculumns', CurriculumViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
