5c40d5f6858ecc7b927166d9ddf3bd4c88e41e9c
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-31 15:51:49 +0100

    Add project view set to rest routers

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,15 @@
 from rest_framework import routers
 from django.conf.urls import include
 from django.urls import path
-from .views import getSiteConfigurations, SocialMediaViewSet, Main_TechViewSet, TechnologyViewSet, CurriculumViewSet
+from .views import getSiteConfigurations, SocialMediaViewSet, Main_TechViewSet, TechnologyViewSet, \
+    CurriculumViewSet, ProjectViewSet
 
 router = routers.DefaultRouter()
 router.register('socialMedias', SocialMediaViewSet)
 router.register('technologies', TechnologyViewSet)
 router.register('mainTech', Main_TechViewSet)
 router.register('curriculumns', CurriculumViewSet)
+router.register('projects', ProjectViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
