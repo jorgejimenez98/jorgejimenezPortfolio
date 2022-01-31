e8b43126a02f8913672ea53539d2fbb6cf695dea
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-31 15:21:41 +0100

    Add Curriculum model to admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from solo.admin import SingletonModelAdmin
-from .models import SiteConfiguration, SocialMedia, Main_Tech, Technologie
+from .models import SiteConfiguration, SocialMedia, Main_Tech, Technologie, Curriculum
 
 
 admin.site.register(SiteConfiguration, SingletonModelAdmin)
 admin.site.register(SocialMedia)
 admin.site.register(Main_Tech)
 admin.site.register(Technologie)
+admin.site.register(Curriculum)
