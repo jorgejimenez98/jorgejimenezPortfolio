4da7cf631da272320f3879af145bf0ead4f8775a
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-05-01 22:55:03 +0200

    feat(populate-database): add errors tratement on create site configs

--- backend/main_settings/management/commands/populate_database.py
+++ backend/main_settings/management/commands/populate_database.py
@@ -15,14 +15,25 @@ class Command(BaseCommand):
     def _create_site_configurations(self):
         self.stdout.write(self.style.NOTICE('Creating Site Configurations !!!!'))
         # Configurations
-        configs = SiteConfiguration.objects.get()
-        configs.developer_name = "Jorge Jimenez"
-        configs.years_of_experience = 2.5
-        configs.is_open_to_work = True
-        configs.phone_number = '653513286'
-        configs.repository_project_url = "https://github.com/jorgejimenez98/jorgejimenezPortfolio"
-        configs.developer_email = "georgeguitarra07@gmail.com"
-        configs.profilePicture = 'profile/foto.jpg'
+        try:
+            configs = SiteConfiguration.objects.get()
+            configs.developer_name = "Jorge Jimenez"
+            configs.years_of_experience = 2.5
+            configs.is_open_to_work = True
+            configs.phone_number = '653513286'
+            configs.repository_project_url = "https://github.com/jorgejimenez98/jorgejimenezPortfolio"
+            configs.developer_email = "georgeguitarra07@gmail.com"
+            configs.profilePicture = 'profile/foto.jpg'
+        except SiteConfiguration.DoesNotExist:
+            configs = SiteConfiguration.objects.create(
+                developer_name='Jorge Jimenez',
+                years_of_experience=2.5,
+                is_open_to_work=True,
+                phone_number='653513286',
+                repository_project_url='https://github.com/jorgejimenez98/jorgejimenezPortfolio',
+                developer_email='georgeguitarra07@gmail.com',
+                profilePicture='profile/foto.jpg'
+            )
         # Translation Texts
         text_spanish = TranslationText.objects.create(language=ENGLISH, text="I am a software developer with 2 years 🚀 of full-stack experience with Django and React, with the ability to develop web systems and android applications. I enjoy working as a team, I'm self-motivated, fast learner, strong reasoning, problem solving skills, and skilled in creating clean, proven and efficient code")
         text_english = TranslationText.objects.create(language=SPANISH, text="Soy desarrollador de software con 2 años de experiencia 🚀 como full-stack con Django y React, con capacidad para desarrollar aplicaciones web y android (Java). Disfruto trabajando en equipo, soy automotivado, de aprendizaje rápido, razonamiento sólido, habilidades para resolver problemas y estoy capacitado para crear código limpio, probado y eficiente.")
