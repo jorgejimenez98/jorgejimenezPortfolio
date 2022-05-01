465755c685cf5d79262cf8d85df4fbafadcfc5c7
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-05-02 01:19:58 +0200

    reformat(social media): read data from backend

--- src/containers/SocialMedia.jsx
+++ src/containers/SocialMedia.jsx
@@ -1,72 +1,56 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Button } from "@mui/material";
+import { Loader } from "../containers";
+import { getSocialMedias } from "../redux/actions";
 import CVDialog from "../components/CVDialog/CVDialog";
 
 export default function SocialMedia({ showResumeButtom }) {
+  const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const { portfolio } = useSelector((state) => state.languaje);
+  const { socialMedias, loading } = useSelector((state) => state.socialMedias);
+
+  useEffect(() => {
+    dispatch(getSocialMedias());
+  }, [dispatch]);
 
   const handleClose = () => {
     setOpen(false);
   };
 
-  if (!portfolio.socialMediaLinks.display) {
-    return null;
-  }
   return (
     <div className="social-media-div">
-      <a
-        href={portfolio.socialMediaLinks.github}
-        className="icon-button github"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fab fa-github"></i>
-        <span></span>
-      </a>
+      {loading ? (
+        <Loader />
+      ) : (
+        socialMedias.length > 0 && (
+          <React.Fragment>
+            {socialMedias.map((item) => (
+              <a
+                href={item.link}
+                className={item.className}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className={item.iconClassName}></i>
+              </a>
+            ))}
 
-      <a
-        href={`mailto:${portfolio.socialMediaLinks.gmail}`}
-        className="icon-button google"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fab fa-google"></i>
-        <span></span>
-      </a>
+            {showResumeButtom && (
+              <Button
+                variant="contained"
+                onClick={() => setOpen(true)}
+                style={{ marginLeft: "25px" }}
+              >
+                {portfolio.labels.cv_button}
+              </Button>
+            )}
 
-      <a
-        href={portfolio.socialMediaLinks.linkedin}
-        className="icon-button linkedin"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fab fa-linkedin-in"></i>
-        <span></span>
-      </a>
-
-      <a
-        href={portfolio.socialMediaLinks.facebook}
-        className="icon-button facebook"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fab fa-facebook-f"></i>
-        <span></span>
-      </a>
-
-      {showResumeButtom && (
-        <Button
-          variant="contained"
-          onClick={() => setOpen(true)}
-          style={{ marginLeft: "25px" }}
-        >
-          {portfolio.labels.cv_button}
-        </Button>
+            <CVDialog open={open} handleClose={handleClose} />
+          </React.Fragment>
+        )
       )}
-
-      <CVDialog open={open} handleClose={handleClose} />
     </div>
   );
 }
